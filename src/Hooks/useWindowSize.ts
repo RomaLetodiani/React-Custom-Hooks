a33963e3ff9b9edb1b697dfce667436fac71596a
RomaLetodiani <roman.letodiani03@gmail.com>
2024-02-23 00:11:48 +0400

    Create useWindowSize.ts

---
+++ src/Hooks/useWindowSize.ts
@@ -0,0 +1,33 @@
+import { useState, useEffect } from 'react';
+
+interface WindowSize {
+  width: number;
+  height: number;
+}
+
+export default function useWindowSize(): WindowSize {
+  const [windowSize, setWindowSize] = useState<WindowSize>({
+    width: window.innerWidth,
+    height: window.innerHeight,
+  });
+
+  useEffect(() => {
+    const handleResize = (): void => {
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  return windowSize;
+}
+
+// Example usage
+// const { height, width } = useWindowSize();
