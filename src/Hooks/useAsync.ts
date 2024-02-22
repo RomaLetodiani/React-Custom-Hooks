aa9cf1b27128ba485a63a4f4b10c88681a758ccd
RomaLetodiani <roman.letodiani03@gmail.com>
2024-02-23 00:28:56 +0400

    Create useAsync.ts

---
+++ src/Hooks/useAsync.ts
@@ -0,0 +1,59 @@
+import { useCallback, useEffect, useState } from 'react';
+
+interface AsyncState<T> {
+  loading: boolean;
+  error?: any;
+  value?: T;
+}
+
+export default function useAsync<T>(callback: () => Promise<T>, dependencies: any[] = []): AsyncState<T> {
+  const [asyncState, setAsyncState] = useState<AsyncState<T>>({
+    loading: true,
+    error: undefined,
+    value: undefined,
+  });
+
+  const callbackMemoized = useCallback(() => {
+    setAsyncState({
+      loading: true,
+      error: undefined,
+      value: undefined,
+    });
+
+    callback()
+      .then((value) => {
+        setAsyncState({
+          loading: false,
+          error: undefined,
+          value,
+        });
+      })
+      .catch((error) => {
+        setAsyncState({
+          loading: false,
+          error,
+          value: undefined,
+        });
+      });
+  }, dependencies);
+
+  useEffect(() => {
+    callbackMemoized();
+  }, [callbackMemoized]);
+
+  return asyncState;
+}
+
+// Example usage
+
+// const fetchData = async (): Promise<string> => {
+// Simulate fetching data from an API
+//     return new Promise<string>((resolve, reject) => {
+//       setTimeout(() => {
+// Resolving with some mock data
+//         resolve('Mock data');
+//       }, 1000);
+//     });
+//   };
+
+//   const { loading, error, value } = useAsync<string>(fetchData, []);
